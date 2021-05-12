dc5194db673908a2edf6b27723d36c3a1d9d3b6c
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-05-12 16:35:39 +0200

    inserite classi e stampa a schermo dei dettagli dei prodotti

--- index.php
+++ index.php
@@ -1,7 +1,83 @@
 <?php 
-class product {
+class Product {
+    protected $category;    
+    protected $img;
 
+
+    // costruttore
+    public function __construct($_category, $_img)
+    {
+        $this->category = $_category;          
+        $this->img = $_img;     
+    }
+
+    // metodi
+    public function getCategory()
+    {
+        return $this->category;
+    }
+    public function getImg()
+    {
+        return $this->img;
+    }   
 }
 
+class Guitar extends Product {
+    public $brand;
+    public $model;
+    protected $price;   
+    // costruttore
+    public function __construct($_category, $_img, $_brand, $_model, $_price)
+    {
+        parent::__construct($_category, $_img);
+        $this->brand = $_brand;
+        $this->model = $_model;        
+        $this->price = $_price;
+    }
+    // metodi
+    public function getCategory()
+    {
+        return $this->category;
+    }
+    public function getImg()
+    {
+        return $this->img;
+    }    
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
+
+$guitar_1 = new Guitar('Chitarra', 'https://d1aeri3ty3izns.cloudfront.net/media/53/533861/600/preview.jpg', 'Gibson', 'Les Paul', 3500);
+$guitar_2 = new Guitar('Chitarra', 'https://thumbs.static-thomann.de/thumb/orig/pics/prod/496368.jpg', 'Fender', 'Stratocaster', 1250);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Prodotti</title>
+    </head>
+    <body>
+        <h2>Single Cut</h2>
+        <ul>
+            <li><?php echo $guitar_2->getCategory()?></li>
+            <li><?php echo $guitar_1->brand?></li>
+            <li><?php echo $guitar_1->model?></li>
+            <li><?php echo $guitar_1->getPrice()?> €</li>
+            <img src="<?php echo $guitar_1->getImg()?>" alt="">
+        </ul>
+        <h2>Strato Style</h2>
+        <ul>
+            <li><?php echo $guitar_2->getCategory()?></li>
+            <li><?php echo $guitar_2->brand?></li>
+            <li><?php echo $guitar_2->model?></li>
+            <li><?php echo $guitar_2->getPrice()?> €</li>
+            <img src="<?php echo $guitar_2->getImg()?>" alt="">
+        </ul>
+    </body>
+</html>
